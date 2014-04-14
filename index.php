c7075e3d900377084aa1ca0adedcee29a63223f8
chris ohara <crsohara@gmail.com>
2014-04-14 20:33:59 +0300

    ignore...

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 	</head>
 	<body>
-		<div class='container'>
+	 	<div class='container'>
 			<header class="page-header">
 				<div class="logo" >
 					<span class="logoBrace">{</span>
